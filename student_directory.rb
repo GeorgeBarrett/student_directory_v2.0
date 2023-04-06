2f0b7c8f8f667c99ea0a5392ba8bea633fcf2f39
george <georgeaustinbarrett@gmail.com>
2023-04-06 16:36:10 +0100

    method created to display the initial user options

--- student_directory.rb
+++ student_directory.rb
@@ -13,3 +13,11 @@
   "november",
   "december"
 ]
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "3. Save the list"
+  puts "4. Load the list"
+  puts "9. Exit"
+end
